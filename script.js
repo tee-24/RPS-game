ee7cc623fc992ad73149f6226e46f940ad7a4188
Towbie Idowu <towbieidowu@gmail.com>
2024-01-29 12:58:39 +0000

    updates

--- script.js
+++ script.js
@@ -2,9 +2,32 @@
 // Get the button elements and add event listeners to them
 
 let help = document.getElementById('instructions');
+let game = document.getElementById('start-game');
+let playerOptions = `<button>1 Player</button>
+<button>2 Players</button>`
 
 if (help) {
     help.addEventListener('click', () => {
         alert("You clicked instructions!");
-    }); 
+    });
+} else if (game) {
+    game.addEventListener('click', () => {
+        alert("You clicked start!");
+        // numberOfPlayers();
+    });
+
+
+    // for (let playerNumber of playerOptions) {
+    //     playerNumber.addEventListener('click', () => {
+    //         userChoice = (choice.textContent).toLowerCase();
+    //         document.getElementById('user-choice').textContent = `You chose: ${userChoice}`;
+
+    //     })
+    // }
+}
+
+function numberOfPlayers() {
+    let options = document.getElementsByClassName('game-area');
+
+    options.innerHTML = playerOptions;
 }
