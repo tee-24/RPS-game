fade03477bd5da48cd12b47344f060c68e3b517e
Towbie Idowu <towbieidowu@gmail.com>
2024-01-31 02:03:12 +0000

    add styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,11 @@ let help = document.getElementById('instructions');
 let game = document.getElementById('start-game');
 
 
-if (help) {
-    help.addEventListener('click', () => {
-        alert("You clicked instructions!");
-    });
-}
+// if (help) {
+//     help.addEventListener('click', () => {
+//         alert("You clicked instructions!");
+//     });
+// }
 
 if (game) {
     game.addEventListener('click', () => {
@@ -21,3 +21,30 @@ if (game) {
         options.innerHTML = playerOptions;
     }); 
 }
+
+
+// Get the modal
+// var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var openButton = document.getElementById("instructios");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+openButton.addEventListener('click', () => {
+    help.style.display = "block";
+  });
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    help.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+      help.style.display = "none";
+    }
+  }); 
