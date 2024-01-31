fc13e657d4bee84049632cbbd17603f3e71d7379
Towbie Idowu <towbieidowu@gmail.com>
2024-01-31 15:30:53 +0000

    modal done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,37 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-let help = document.getElementById('instructions');
-let game = document.getElementById('start-game');
+var game = document.getElementById('start-game');
 
+game.addEventListener('click', () => {
 
-// if (help) {
-//     help.addEventListener('click', () => {
-//         alert("You clicked instructions!");
-//     });
-// }
+  let options = document.querySelector('.game-area');
+  let playerOptions = `<a href="game.html"><button>1 Player</button></a>
+  <a href="twoplayer.html"><button>2 Players</button></a>`;
 
-if (game) {
-    game.addEventListener('click', () => {
-        let options = document.querySelector('.game-area');
-        let playerOptions = `<a href="game.html"><button>1 Player</button></a>
-        <a href="twoplayer.html"><button>2 Players</button></a>`;
+  options.innerHTML = playerOptions;
+}); 
 
-
-        options.innerHTML = playerOptions;
-    }); 
-}
-
+// Open modal when clicked
+var instruction = document.getElementById('instructions');
 
 // Get the modal
-// var modal = document.getElementById("myModal");
+var modal = document.getElementById("help");
 
-// Get the button that opens the modal
-var openButton = document.getElementById("instructios");
+instruction.addEventListener('click', () => {
+  modal.style.display = 'block';
+})
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-openButton.addEventListener('click', () => {
-    help.style.display = "block";
-  });
+span.addEventListener('click', () => {
+  modal.style.display = 'none';
+})
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    help.style.display = "none";
-  }
-  
   // When the user clicks anywhere outside of the modal, close it
   window.addEventListener('click', function(event) {
-    if (event.target == modal) {
-      help.style.display = "none";
+    if (event.target === modal) {
+      modal.style.display = "none";
     }
   }); 
